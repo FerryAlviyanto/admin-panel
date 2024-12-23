75de00bc60d8f5e0a7d9fb7d96a1f2e0fee3809f
FerryAlviyanto <ferry.alviyanto16@gmail.com>
2024-12-23 21:20:53 +0700

    commit5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 "use client";
+import Banner from "@/components/front-end/Banner";
 import Cart from "@/components/front-end/Cart";
+import CartProduct from "@/components/front-end/CartProduct";
+import Feature from "@/components/front-end/Feature";
+import Footer from "@/components/front-end/Footer";
+import Hero from "@/components/front-end/Hero";
 import Navbar from "@/components/front-end/Navbar";
-import Image from "next/image";
-import { useState } from "react";
+import TrendingProducts from "@/components/front-end/TrendingProduct";
+import React, { useState } from "react";
 
 const Home = () => {
   const [showCart, setShowCart] = useState(false);
@@ -11,6 +16,11 @@ const Home = () => {
     <main>
       <Navbar setShowCart={setShowCart} />
       {showCart && <Cart setShowCart={setShowCart} />}
+      <Hero />
+      <Feature />
+      <TrendingProducts />
+      <Banner />
+      <Footer />
     </main>
   );
 };
